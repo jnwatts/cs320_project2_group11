d966d206baaa0cca908cedbe5ba121cb6f8343a8
Josh Watts <jwatts@d3engineering.com>
2014-04-18 15:51:59 -0400

    MainView: Enable DoubleBuffer, just because

--- mainview.cs
+++ mainview.cs
@@ -19,6 +19,8 @@ public class MainView : Form
     {
         this.SuspendLayout();
 
+        this.DoubleBuffered = true;
+
         tabControl = new TabControl();
         tabControl.Dock = DockStyle.Fill;
         Controls.Add(tabControl);
